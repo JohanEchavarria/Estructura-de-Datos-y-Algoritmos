e49c390e3c695ccedeb97ce0134b39c86163d647
JohanEchavarria <Johan_Ech>
2025-05-06 17:55:57 -0400

    q:

--- TRABAJO FINAL/ejercicio277.py
+++ TRABAJO FINAL/ejercicio277.py
@@ -1,14 +1,5 @@
 import mysql.connector
 
-conexion1=mysql.connector.connect(host="localhost",user="root",password="")
-cursor1=conexion1.cursor()
-cursor1.execute("show databases")
-for base in cursor1:
-    print(base)
-conexion1.close()
-
-import mysql.connector
-
 conexion1=mysql.connector.connect(host="localhost", user="root", passwd="", database="bd1")
 
 cursor1=conexion1.cursor()
